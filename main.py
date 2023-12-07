f12112bc74040feb646ef86b6d53cd03feeccabb
Isaiahensley <143129356+Isaiahensley@users.noreply.github.com>
2023-12-07 10:41:43 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,89 @@
+import streamlit as st
+
+def main():
+    st.title("Aquatic Rig Roadmap")
+
+    # August
+    st.header("Aug 2023 - Sep 2023")
+
+    st.write("✅🕒 Project Idea")
+
+    # September
+    st.header("Sep 2023 - Oct 2023")
+
+    st.write("✅ Key Partners")
+    st.write("✅ Key Activites")
+    st.write("✅ Value Propositions")
+    st.write("✅ Key Resources")
+    st.write("✅ Cost Structure")
+    st.write("✅ Revenue Streams")
+    st.write("✅ Customer Relationships")
+    st.write("✅ Channels")
+    st.write("✅ Customer Segments")
+    st.write("✅ Creating Buisness Model Canvas")
+    st.write("✅ Design the Box Exercise")
+    st.write("✅ Positioning Statement")
+    st.write("✅🕒 Elevator Test Statement")
+    st.write("✅ Information Gathering")
+    st.write("✅ Early Architectural Skeleton")
+    st.write("✅🕒 Story Board")
+    st.write("✅ Business Plan")
+    st.write("✅ Envisioning Presentation")
+
+
+    # October
+    st.header("Oct 2023 - Nov 2023")
+
+    st.write("✅ Project Management")
+    st.write("✅ Coding and Testing Philosophy")
+    st.write("✅ Product Ownership")
+    st.write("✅🕒 Development Tools")
+    st.write("✅🕒 Roadmap")
+    st.write("✅🕒 Story Board Updated")
+    st.write("✅ Automated Deployment Pipeline")
+    st.write("✅ October Retrospective")
+    st.write("✅ Speculating Presentation")
+
+
+
+    # November
+    st.markdown(
+        '<p style="background-color:yellow; font-size: 2em;"><strong>Nov 2023 - Dec 2023</strong></p>',
+        unsafe_allow_html=True)
+
+    # November
+    #st.header("Nov 2023 - Dec 2023")
+
+    st.write("✅ Deployment Stages")
+    st.write("✅ Error Tracking")
+    st.write("✅ Gitflow")
+    st.write("✅ Development Tool Descriptions")
+    st.write("✅ CI/CD")
+    st.write("✅ November Retrospective")
+    st.write("✅ Pipeline Presentation")
+
+
+
+    # January
+    st.header("Jan 2024 - Feb 2024")
+    st.write("❌ Development Planning")
+    st.write("•••")
+
+    # February
+    st.header("Feb 2024 - Mar 2024")
+    st.write("❌ Alpha Development")
+    st.write("•••")
+
+    # March
+    st.header("Mar 2024 - Apr 2024")
+    st.write("❌ Beta Development")
+    st.write("•••")
+
+    # April
+    st.header("Apr 2024 - May 2024")
+    st.write("❌ General Availabilty Development")
+    st.write("•••")
+
+
+if __name__ == "__main__":
+    main()
